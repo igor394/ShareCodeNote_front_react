5f7dfe926d2391ef1b793ddfd988f1796c495c8a
igor349 <igorsvidunovic@gmail.com>
2021-03-10 10:40:34 +0200

    component 'Create_OOP'complited and save

--- src/components/Create_OOP.js
+++ src/components/Create_OOP.js
@@ -1,13 +1,72 @@
 import React from 'react';
-import { useState } from 'react';
 import env from '../env.json';
 
 class Create extends React.Component {
-    render() {
+    constructor() {
+        super();
+        this.state = {
+            url: '',
+            lineClass: 'hide',
+            formClass: ''
+        }
+    }
 
+    loadDataForm=(event)=>{
+        event.preventDefault();
+        let note = event.target.elements.note.value;
+        note = note.trim();
+        if (note === ''){
+            alert('Please, fill the field!');
+            return false;
+        } else {
+            this.sendData(note);
+        }
+    }
+    sendData=(note)=>{
+        this.setState({
+            formClass: 'hide',
+            lineClass: ''
+        });
+
+        fetch(env.urlBackend, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: JSON.stringify({note: note})
+        })
+            .then(res=>res.json())
+            .then(res=>{
+                if(res.result) this.setState({url: env.url + '/' + res.url});
+            })
+    }
+
+    render() {
         return (
             <div className="container content-pad">
-
+                <form onSubmit={this.loadDataForm} className={this.state.formClass}>
+                    <div className="mb-3">
+                        <label className="form-label">Enter you text</label>
+                    </div>
+                    <div className="mb-3">
+                            <textarea className="form-control" name="note" id="note" defaultValue="Test coded note"
+                                      maxLength="500"></textarea>
+                    </div>
+                    <div className="mb-3">
+                        <p><b>Attention!</b> Note length is limited to a maximum value - 1000 characters.</p>
+                        <button type="submit" className="btn btn-secondary ">Create</button>
+                    </div>
+                </form>
+                <div className={this.state.lineClass}>
+                    <div className="mb-3">
+                        <div className="alert alert-secondary" role="alert">{this.state.url}</div>
+                        <p>Copy this URL and give it to your friend!</p>
+                    </div>
+                    <div className="mb-3">
+                        <button className="btn btn-secondary" onClick={() => {
+                            window.location.reload()
+                        }}>Create new link
+                        </button>
+                    </div>
+                </div>
             </div>
         )
     }
@@ -15,11 +74,7 @@ class Create extends React.Component {
 
 export default Create;
 
-// function Create() {
 
-//     const [url, setUrl] = useState('');
-//     const [line, setLine] = useState('hide');
-//     const [formClass, setFormClass] = useState('')
 
 //     const sendData = note => {
 //         setLine('');
@@ -51,28 +106,3 @@ export default Create;
 //         sendData(note);
 //     }
 
-                // <form onSubmit={loadDataFromForm} className={formClass}>
-                //         <div className="mb-3">
-                //             <label className="form-label">Enter you text</label>
-                //         </div>
-                //         <div className="mb-3">
-                //             <textarea className="form-control" name="note" id="note" defaultValue="Test coded note"
-                //                 maxLength="500"></textarea>
-                //         </div>
-                //         <div className="mb-3">
-                //             <p><b>Attention!</b> Note length is limited to a maximum value - 1000 characters.</p>
-                //             <button type="submit" className="btn btn-secondary ">Create</button>
-                //         </div>
-                //     </form>
-                //     <div className={line}>
-                //         <div className="mb-3">
-                //             <div className="alert alert-secondary" role="alert">{url}</div>
-                //             <p>Copy this URL and give it to your friend!</p>
-                //         </div>
-                //         <div className="mb-3">
-                //             <button className="btn btn-secondary" onClick={() => {
-                //                 window.location.reload()
-                //             }}>Create new link
-                //     </button>
-                //         </div>
-                //     </div>
